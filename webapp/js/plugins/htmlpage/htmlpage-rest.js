93663624a39d2c43d1f49c8eb4f7aed1e834a715
HOHL <laurent.hohl@gmail.com>
2023-05-15 14:52:47 +0200

    LUT-25209: Add divClass param to add css class to target div

--- webapp/js/plugins/htmlpage/htmlpage-rest.js
+++ webapp/js/plugins/htmlpage/htmlpage-rest.js
@@ -7,7 +7,7 @@ function setBaseUrl( baseUrl )
 	}
 }
 
-function getContentHtmlPage( id, idDefault, idDiv  ){
+function getContentHtmlPage( id, idDefault, idDiv, divClass ){
     var url = _baseUrl + "rest/htmlpage-api/v1/htmlpage/" + id;
     var data = {id_default:idDefault};
     $.ajax({
@@ -17,7 +17,8 @@ function getContentHtmlPage( id, idDefault, idDiv  ){
         data: data,
         success: function (data) {
             if ( data.status == 'OK' ) {
-            	$("#"+ idDiv ).html( data.result );
+				if( divClass != undefined ){ $("#"+ idDiv ).addClass( 'htmlpage' ) };
+            	$( "#"+ idDiv ).html( data.result ); 
             }
         },
         error: function (jqXHR, textStatus, errorThrown) {
