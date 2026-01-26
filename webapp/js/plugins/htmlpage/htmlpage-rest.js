5f6da1cef3cabce791fa60589d8e01a384656a6e
hohll <laurent.hohl@gmail.com>
2026-01-26 10:11:29 +0100

    LUT-29276 : Remove JQuery

--- webapp/js/plugins/htmlpage/htmlpage-rest.js
+++ webapp/js/plugins/htmlpage/htmlpage-rest.js
@@ -1,27 +1,36 @@
 var _baseUrl = document.getElementsByTagName('base')[0].href;
 
-function setBaseUrl( baseUrl )
-{
-	if( baseUrl != '' ){
-		_baseUrl = baseUrl;
-	}
+function setBaseUrl( baseUrl ){
+    if( baseUrl != '' ){
+        _baseUrl = baseUrl;
+    }
 }
 
 function getContentHtmlPage( id, idDefault, idDiv, divClass ){
     var url = _baseUrl + "rest/htmlpage-api/v1/htmlpage/" + id;
-    var data = {id_default:idDefault};
-    $.ajax({
-        url: url,
-        type: "GET",
-        dataType: "json",
-        data: data,
-        success: function (data) {
-            if ( data.status == 'OK' ) {
-				if( divClass != undefined ){ $("#"+ idDiv ).addClass( 'htmlpage' ) };
-            	$( "#"+ idDiv ).html( data.result ); 
-            }
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
+    if( idDefault ){
+        url += "?id_default=" + encodeURIComponent(idDefault);
+    }
+    
+    fetch(url, {
+        method: "GET",
+        headers: {
+            "Accept": "application/json"
         }
-    });   
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        if ( data.status == 'OK' ) {
+            var divElement = document.getElementById(idDiv);
+            if( divClass != undefined ){ 
+                divElement.classList.add('htmlpage');
+            }
+            divElement.innerHTML = data.result;
+        }
+    })
+    .catch(function(error) {
+        console.error('Error:', error);
+    });
 }
